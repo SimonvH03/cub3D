7052fc2a6fc2cbd6b0adc3f4154068e0fe8d8284
simon <si.hoogstraaten86@gmail.com>
2024-08-26 19:08:19 +0200

    ready to start

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: simon <simon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 18:33:20 by svan-hoo          #+#    #+#             */
-/*   Updated: 2024/08/26 18:18:32 by simon            ###   ########.fr       */
+/*   Updated: 2024/08/26 19:08:08 by simon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,37 +17,38 @@
 // 			user_inputs
 // Phase 3: project
 // Phase 4: draw
-// static void
-// 	loop_hooks(
-// 		t_cub3d *cub3d)
-// {
-// 	mlx_key_hook(cub3d->mlx, &keyhook, cub3d);
-// 	mlx_scroll_hook(cub3d->mlx, &scrollhook, cub3d);
-// 	mlx_loop_hook(cub3d->mlx, &user_inputs, cub3d);
-// 	mlx_loop_hook(cub3d->mlx, &project, cub3d);
-// 	mlx_loop_hook(cub3d->mlx, &draw, cub3d);
-// }
+static void
+	loop_hooks(
+		t_cub3d *cub3d)
+{
+	mlx_key_hook(cub3d->mlx, &keyhook, cub3d);
+	mlx_scroll_hook(cub3d->mlx, &scrollhook, cub3d);
+	mlx_loop_hook(cub3d->mlx, &user_inputs, cub3d);
+	mlx_loop_hook(cub3d->mlx, &project, cub3d);
+	mlx_loop_hook(cub3d->mlx, &draw, cub3d);
+}
 
 // Phase 1: map_init
 // 			cub3d_init
 // 			menu_draw
 int
 	main(
-		void)
+		int argc,
+		char **argv)
 {
-	// t_map			map;
-	// t_cub3d			cub3d;
+	t_map			map;
+	t_cub3d			cub3d;
 
-	// if (argc != 2)
-	// 	return (EXIT_FAILURE);
-	// if (map_init(&map, argv[1]) == EXIT_FAILURE)
-	// 	return (EXIT_FAILURE);
-	// if (cub3d_init(&cub3d, &map) == EXIT_FAILURE)
-	// 	return (EXIT_FAILURE);
-	// menu_draw(&cub3d);
-	// loop_hooks(&cub3d);
-	// mlx_loop(cub3d.mlx);
-	// mlx_terminate(cub3d.mlx);
-	// map_free(&map);
+	if (argc != 2)
+		return (EXIT_FAILURE);
+	if (map_init(&map, argv[1]) == EXIT_FAILURE)
+		return (EXIT_FAILURE);
+	if (cub3d_init(&cub3d, &map) == EXIT_FAILURE)
+		return (EXIT_FAILURE);
+	menu_draw(&cub3d);
+	loop_hooks(&cub3d);
+	mlx_loop(cub3d.mlx);
+	mlx_terminate(cub3d.mlx);
+	map_free(&map);
 	return (EXIT_SUCCESS);
 }
